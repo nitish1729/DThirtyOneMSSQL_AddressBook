afe1b4682171de42a5baaa65a4a754fcb78c781e
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-23 00:55:38 +0530

    Created AddressBook table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,15 @@
 create database AddressBook_DB;
 use AddressBook_DB;
+
+CREATE TABLE AddressBook
+(
+ FirstName varchar(50),
+ LastName varchar(50),
+ Address varchar (250),
+ City varchar (100),
+ state varchar (100),
+ ZipCode float,
+ PhoneNumber float,
+ EmailID varchar(100)
+);
+
